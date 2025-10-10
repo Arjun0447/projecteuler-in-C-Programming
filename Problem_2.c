9796d376b3c235e390a3bd7c385dc3a913546df1
Arjun Sharma <arjunsharm2003@gmail.com>
2025-10-10 07:56:37 +0530

    Update Problem_2.c

--- Problem_2.c
+++ Problem_2.c
@@ -7,25 +7,20 @@ int main() {
     
     int a = 1; //Initial Term 1
     int b = 2; //Initial Term 2
-    
     long sum = 2;
     
     while (1) {
-        int next_term = a + b;
-        
+        int next_term = a + b; 
         if (next_term > limit) { //Limit Declaration
             break;
         }
-        
         if (next_term % 2 == 0) { //Check if the term is even
             sum += next_term; //Add the even term to the sum
         }
-        
         a = b;
         b = next_term;
     }
-    
     printf("Sum of Fibonacci series is: %ld\n", sum);
-    
     return 0;
+
 }
